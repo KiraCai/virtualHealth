6ef564a3b29b0a250de48c7164706cf3b43ac989
Kira Korelskaia <kiraswav@gmail.com>
2024-12-23 21:53:03 +0100

    with sees the file App

--- application/index.js
+++ application/index.js
@@ -1,12 +1,16 @@
 //import { StrictMode } from 'react';
-//import { createRoot } from 'react-dom/client';
+
 import './index.css';
-//import './App.js';
-//import MyApp from './App.js';
 import './index.html';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import MyApp from './App';
+const div = document.createElement('div');
+div.setAttribute('id', 'app');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+document.body.appendChild(div);
+const root = createRoot(document.querySelector('#app'));
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(<MyApp />);
---
+++ dist/9227b16733094b935b2d.js
@@ -0,0 +1,1 @@
+import React from"react";export default function MyApp(){return React.createElement("div",null,React.createElement("h1",null,"Welcome to my app"))}
