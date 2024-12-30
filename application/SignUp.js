466cdb467087a4728940d5962df8737661359fdd
Kira Korelskaia <kiraswav@gmail.com>
2024-12-30 22:45:54 +0100

    Start of registration page

--- application/SignUp.js
+++ application/SignUp.js
@@ -3,9 +3,52 @@ import React, { useState } from 'react';
 const SignUp = () => {
   console.log('SignUp component rendered');
   return (
-    <div>
-      <h1 className="sign">КУКУ</h1>
-    </div>
+    <main className="mainSign thin">
+      <div className="signPage fat">
+        <h1>Inscription</h1>
+        <div>
+          <form id="signForm">
+            <div class="input-box">
+              <label>
+                Nom
+                <input type="text" name="name" />
+              </label>
+            </div>
+            <div class="input-box">
+              <label>
+                Prénom
+                <input type="text" name="surname" />
+              </label>
+            </div>
+            <div class="input-box">
+              <label>
+                e-mail
+                <input type="text" name="mail" />
+              </label>
+            </div>
+            <div class="input-box">
+              <label>
+                Téléphone
+                <input type="text" name="tel" />
+              </label>
+            </div>
+            <div class="input-box">
+              <label>
+                Mot de passe
+                <input type="text" name="password" />
+              </label>
+            </div>
+            <div class="input-box">
+              <label>
+                Répéter le mot de passe
+                <input type="text" name="repeatPassword" />
+              </label>
+            </div>
+            <input type="submit" value="Envoyer" />
+          </form>
+        </div>
+      </div>
+    </main>
   );
 };
 export default SignUp;
